96afae866c4cce8124afa6b8260afb2ae52187b8
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-08-09 22:32:21 +0300

    feat: added new examples to logging

---
+++ logging/logging_format.py
@@ -0,0 +1,5 @@
+import logging
+logging.basicConfig(format='%(asctime)s|%(message)s')
+logging.debug('This message should appear on the console')
+logging.info('So should this')
+logging.warning('And this, too')
--- logging/logging_variable_data.py
+++ logging/logging_variable_data.py
@@ -1,2 +1,5 @@
 import logging
+import time
+
 logging.warning('%s before you %s', 'Look', 'leap!')
+logging.error(f'{time.time()} this is time.time()')
